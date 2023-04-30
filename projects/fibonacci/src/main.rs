b82f7f6c6019ea0b1409c58645f40c15eb300bf2
Charles Ott <charlieott@gmail.com>
2023-04-29 22:23:29 -0400

    Created a Fibonacci Sequence Generator.

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    //In mathematics, the Fibonacci sequence is a sequence in which
+    //each number is the sum of the two preceding ones.
+    
+    println!("Hello, Fibonacci!");
+  
+    // The nth Number in a sequence...
+    let n: u32 = 32;
+
+    // A tuple to store the two preceding values.
+    let mut tup: (u32, u32) = (0, 1);
+  
+    // Loop Counter
+    let mut count = 1;
+    loop {
+        let sum = tup.0 + tup.1; 
+        tup.0 = tup.1;
+        tup.1 = sum;
+        println!("Fibonacci Sequence #{count} is  {sum}.");
+        if count == n {
+            break;
+        }
+        count += 1;
+    }
 }
