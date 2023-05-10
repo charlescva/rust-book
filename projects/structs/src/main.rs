91b96fc80aaa66586d9a8771cdffb680b153a352
Charles Ott <charlieott@gmail.com>
2023-05-09 20:30:40 -0400

    Applying printlines to structs

--- projects/structs/src/main.rs
+++ projects/structs/src/main.rs
@@ -17,6 +17,7 @@ fn main() {
         sign_in_count: 1,
     };
 
+    println!("Created a new user named, {}",user1.username);
     // Listing 5-3: Creating an instance of the User struct
 
     let mut user1 = User {
@@ -25,10 +26,23 @@ fn main() {
         email: String::from("failboat@sadc2.org"),
         sign_in_count: 1,
     };
+    println!("Mutable User's initial email, {}", user1.email);
 
     user1.email = String::from("anotheremail@sadc2.org");
 
+    println!("Mutable user's new email is, {}", user1.email);
 
+    // Listing 5-7: Using struct update syntax to set email
+    let user2 = User {
+        email: String::from("anotheruser@example.com"),
+        ..user1
+    };
+
+    println!("This user was created with struct update syntax!\r\n{}", user2.username);
+
+    println!("Building a new user...");
+    let user3 = build_user(String::from("functionaluser@gmail.com"), String::from("funcy_user"));
+    println!("Signin Count: {}, Status: {}", user3.sign_in_count, user3.active);
 }
 
 // Listing 5-4: A build_user fn that takes an email and username
@@ -38,7 +52,7 @@ fn build_user(email: String, username: String) -> User {
     User {
         active: true,
         username: username,
-        email: email,
+        email, // Field Init Shorthand
         sign_in_count: 1,
     }
 
